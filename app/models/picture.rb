77385100b9b5df76ea47b6aeba4bcdec168e2ed7
fujitamariko <fujyta.mrk@gmail.com>
2021-08-26 16:29:53 +0900

    お気に入り機能の設定

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,6 @@
 class Picture < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
 end
